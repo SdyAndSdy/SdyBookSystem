f206c5e5a885f55bf2fcb3e9635e4462d72fe8a0
Administrator <Administrator@PC-202003261650>
2020-06-17 17:32:47 +0800

    ssss

--- BookManagerSystem/src/main/java/com/hq/dao/MasterDao.java
+++
@@ -1,9 +0,0 @@
-package com.hq.dao;
-
-import com.hq.bean.SysMaster;
-
-public interface MasterDao {
-	//根据用户名和密码查询用户
-	public SysMaster selectByaccountAndPassword(String account,String password);
-
-}
--- BookManagerSystem/src/main/java/com/hq/service/MasterService.java
+++
@@ -1,6 +0,0 @@
-package com.hq.service;
-
-public interface MasterService {
-	public boolean LoginMaster(String account,String password1);
-
-}
